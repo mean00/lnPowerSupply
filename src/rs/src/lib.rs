25a058f32b89f1b511e0103a0efb09d382c1e2cb
mean <fixounet@free.fr>
2022-08-18 18:14:11 +0200

    add dummy rust payload

---
+++ src/rs/src/lib.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+use core::panic::PanicInfo;
+
+use rnarduino as rn;
+
+#[no_mangle]
+pub extern "C" fn rnLoop() -> ()
+{
+   // eventGroup->takeOwnership();
+  
+}
