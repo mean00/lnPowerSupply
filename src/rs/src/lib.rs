a74c473bb55a88215040c877b1c1e15a011929cd
mean <fixounet@free.fr>
2022-08-20 07:44:10 +0200

    unsafe everything

--- src/rs/src/lib.rs
+++ src/rs/src/lib.rs
@@ -3,13 +3,38 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-use core::panic::PanicInfo;
-
+//use lazy_static::lazy_static;
 use rnarduino as rn;
+
+
+//extern "C" {
+  // static mut eventGroup: *mut rn::lnFastEventGroup;
+//}
+
+//static  eventGrp : & 'static   rn::lnFastEventGroup = unsafe { &mut *eventGroup as &mut  rn::lnFastEventGroup  };
+
+extern "C" {
+   static mut eventGroup: &'static mut rn::lnFastEventGroup;
+}
+
+
 
 #[no_mangle]
 pub extern "C" fn rnLoop() -> ()
 {
-   // eventGroup->takeOwnership();
+   unsafe{
+      eventGroup.takeOwnership();
+   }
+   //tsk=createI2cTask(i2cCb);
+   //xDelay(20); // let it start    
+   //float vbat;
+   //int imaxAmp, lastMaxCurrent=-1;;
+   
+   //runAdc(vbat, imaxAmp);
+   //if(vbat<PS_MIN_VBAT)
+   //{
+   //    stopLowVoltage();
+   //}    
+
   
 }
