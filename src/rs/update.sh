6a2282e8cc6cfafd829a44145ae70f55dd0e5f9c
mean <fixounet@free.fr>
2022-09-01 07:43:29 +0200

    add bindgen script

---
+++ src/rs/update.sh
@@ -0,0 +1,3 @@
+ bash ../../../lnArduino/cmake/rustgen.sh  ../../inc/lnDisplay.h  src/display.rs
+ #bash ../../../lnArduino/cmake/rustgen.sh  ../../inc/lnI2CTask.h  src/i2cTask.rs
+ bash ../../../lnArduino/cmake/rustgen.sh  ../../src/cpp/i2cTask_shim.h  src/i2cTask.rs
