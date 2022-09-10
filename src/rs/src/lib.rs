340c8ca7ab5e94ac49abfdb063d9c0686d82de9f
mean <fixounet@free.fr>
2022-09-10 08:12:41 +0200

    switch to clang, cleanup bindgen stuff

---
+++ src/rs/src/lib.rs
@@ -0,0 +1,13 @@
+#![no_std]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+#![feature(default_alloc_error_handler)]
+
+
+mod display;
+mod i2cTask;
+mod settings;
+mod app;
+// EOF
