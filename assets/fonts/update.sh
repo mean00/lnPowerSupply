4943674a25f6c3a32156ba40efa381999e6d303f
mean <fixounet@free.fr>
2022-09-17 11:10:34 +0200

    update font and convert script

---
+++ assets/fonts/update.sh
@@ -0,0 +1,6 @@
+export EXE=flatconvert-rs
+#export EXE=flatconvert
+${EXE}  -f Waree.ttf -s 12 -o waree12.h -p 2 -c
+${EXE}  -f Roboto-Light.ttf -s 12 -o roboto12.h -p 2 -c
+${EXE}  -f Roboto-Light.ttf -s 28 -o robotoLight.h -p 2 -c
+${EXE}  -f RobotoSlab-SemiBold.ttf  -s 48 -o robotoslab.h -p 2 -c -k "0123456789./AVWm"
