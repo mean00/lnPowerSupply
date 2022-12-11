3c6bedeed0fc27856e68d907d468d9d1330c5c17
mean <fixounet@free.fr>
2022-12-11 07:08:36 +0100

    switch to simpler_gfx font

---
+++ src/rs/src/update_font.sh
@@ -0,0 +1,7 @@
+export EXE=flatconvert-rs
+export D=../../../assets/fonts
+#export EXE=flatconvert
+${EXE}  -f $D/Waree.ttf -s 12 -r waree12.rs -p 2 
+#${EXE}  -f Roboto-Light.ttf -s 12 -o roboto12.h -p 2 -r
+${EXE}  -f $D/Roboto-Light.ttf -s 28 -r robotoLight28.rs -p 2 
+${EXE}  -f $D/RobotoSlab-SemiBold.ttf  -s 48 -r robotoslab48.rs -p 2  -k "0123456789./AVWm"
