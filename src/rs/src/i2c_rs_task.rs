4c9ecdb426488d994df8d3b465b89b94bf8e7bbf
mean <fixounet@free.fr>
2022-12-18 17:48:38 +0100

    flesh out i2c task

--- src/rs/src/i2c_rs_task.rs
+++ src/rs/src/i2c_rs_task.rs
@@ -8,9 +8,6 @@ use rnarduino as rn;
 
 use rn::rnGpio as rnGpio;
 use rn::rnGpio::rnPin as rnPin;
-use rn::rnExti as rnExti;
-use rn::rnFastEventGroup::rnFastEventGroup;
-use rn::rnTimingAdc::rnTimingAdc;
 use rn::rnOsHelper::rnLogger as rnLogger;
 use cty::c_void;
 use crate::settings::{*};
@@ -21,17 +18,26 @@ use mcp4725::MCP4725;
 
 use core::str;
 
+pub enum PeripheralEvent
+{
+    CCChangeEvent=1,
+    VoltageChangeEvent=2,
+}
+//------------------------------------------------------------
+pub trait peripheral_notify
+{
+    fn notify(&self, event :  PeripheralEvent);
+}
 //-----------------------------------------------------------
 pub trait peripheral_interface
 {
 
-            fn  get_voltage() -> f32;
-            fn  get_current_ma() -> usize;
-            fn  set_max_current(milliamp : usize) ;
-            fn  cc_limited() -> bool;
-            fn  dcdc_enable(enable : bool) ;
-            fn  output_enable(enable: bool) ;
-            //fn  setCb(lnI2cTask::signalCb *c) ;
+            fn  get_voltage(&mut self) -> f32;
+            fn  get_current_ma(&mut self) -> usize;
+            fn  set_max_current(&mut self, milliamp : usize) ;
+            fn  cc_limited(&mut self) -> bool;
+            fn  dcdc_enable(&mut self, enable : bool) ;
+            fn  output_enable(&mut self, enable: bool) ;            
 }
 //-----------------------------------------------------------
 pub struct power_supply_peripheral
@@ -40,18 +46,43 @@ pub struct power_supply_peripheral
     ina219   : INA219,
     mcp4725  : MCP4725,
 
+    current_volt            : f32,
+    current_ma              : usize, 
+    current_max_current     : usize,
+    current_dc_enabled      : bool,
+    current_relay_enabled   : bool, 
+    current_cc              : bool,
+
+
+    updated_max_current     : usize,
+    updated_dc_enabled      : bool, 
+    updated_relay_enabled   : bool,
+
+    callback                : &'static dyn peripheral_notify, 
+
 }
 //-----------------------------------------------------------
 impl power_supply_peripheral
 {
-    pub fn new() -> Self
-    {
-        //pub fn rnCreateTask(entry : &rnTaskEntry, name: &str, priority : usize, taskSize : u32)
+    pub fn new( cb : &'static dyn peripheral_notify) -> Self
+    {        
         let mut r = power_supply_peripheral
         {
             pc8574   : PC8754::new(PS_I2C_INSTANCE, IO_EXPANDER_ADDRESS as u8),
             ina219   : INA219::new(PS_I2C_INSTANCE as usize, INA219_ADDRESS as u8,  100*1000, INA219_SHUNT_VALUE ,3),
             mcp4725  : MCP4725::new( PS_I2C_INSTANCE as usize, MCP4725_ADDRESS , 100*1000),
+            current_volt            : 0.,
+            current_ma              : 0, 
+            current_max_current     : 200,
+            current_dc_enabled      : false,
+            current_relay_enabled   : false, 
+            current_cc              : false,
+        
+            callback                : cb,
+        
+            updated_max_current     : 200,
+            updated_dc_enabled      : false, 
+            updated_relay_enabled   : false,
         };
 
         r.start();
@@ -65,48 +96,93 @@ impl power_supply_peripheral
         }
       
     }
-    pub fn run(&mut self)
-    {
-        self.pc8574.write(IO_EXPANDER_DC_ENABLE, false); // DC/DC is active low
-        self.pc8574.write(IO_EXPANDER_RELAY_ENABLE,false);
-        loop
-        {
-            rnLogger("Hey!!\n");
-            rnDelay(1000);
-        }
-    }
+  
     pub fn start(&mut self)
     {        
         let mut myself =  self as *mut _ as *mut c_void;
         rnCreateTask( &(Self::trampoline as rnTaskEntry) , "i2c",I2C_TASK_PRIORITY, 1024, myself);
     }
 
+    fn notify(&mut self, event : PeripheralEvent)
+    {
+        self.callback.notify(event);
+    }
+
+    //----------------------
+    // This will actually look after sensors
+    // and enforce the changes
+    //----------------------
+    pub fn run(&mut self)
+    {
+        self.pc8574.write(IO_EXPANDER_DC_ENABLE, false);    // DC/DC is active low
+        self.pc8574.write(IO_EXPANDER_RELAY_ENABLE,false);  //
+        self.mcp4725.set_voltage(200); // default low value
+        loop
+        {
+            rnDelay(10); 
+            let current = self.ina219.get_current_ma();
+            if current!=self.current_ma
+            {
+                self.current_ma = current;
+                self.notify(PeripheralEvent::VoltageChangeEvent);
+            }
+            // CC ?
+            self.pc8574.refresh();
+            let cc = self.pc8574.read(IO_EXPANDER_CC_MODE);
+            if cc != self.current_cc
+            {
+                self.current_cc = cc;
+                self.notify(PeripheralEvent::CCChangeEvent);
+            }
+            // limit ?
+            let max_current = self.updated_max_current;
+            if max_current != self.current_max_current
+            {
+                    self.current_max_current = max_current;
+                    self.mcp4725.set_voltage(max_current);
+            }
+            let dc_enabled = self.updated_dc_enabled;
+            let relay_enabled = self.updated_relay_enabled;
+
+            // something changed ?
+            if self.current_dc_enabled!=dc_enabled ||  self.current_relay_enabled!=relay_enabled
+            {
+                self.current_dc_enabled  = dc_enabled;
+                self.current_relay_enabled  = relay_enabled;
+                
+                self.pc8574.write(IO_EXPANDER_DC_ENABLE, !self.current_dc_enabled); // active low
+                self.pc8574.write(IO_EXPANDER_RELAY_ENABLE, !self.current_relay_enabled); // active low
+            }
+        }
+    }
 }
 //-----------------------------------------------------------
 impl peripheral_interface for power_supply_peripheral
 {
-    fn  get_voltage() -> f32
+    fn  get_voltage(&mut self) -> f32
     {
-        5.
+        self.current_volt
     }
-    fn  get_current_ma() -> usize
+    fn  get_current_ma(&mut self) -> usize
     {
-        120
+        self.current_ma
     }
-    fn  set_max_current(milliamp : usize)
+    fn  set_max_current(&mut self, milliamp : usize)
     {
-
+        self.updated_max_current=milliamp;
     }
-    fn  cc_limited() -> bool
+    fn  cc_limited(&mut self) -> bool
     {
-        false
+        self.current_cc
     }
-    fn  dcdc_enable(enable : bool)
+    //--
+    fn  dcdc_enable(&mut self, enable : bool)
     {
-        
+        self.updated_dc_enabled = enable;         
     }
-    fn  output_enable(enable: bool)
+    fn  output_enable(&mut self, enable: bool)
     {
-        
-    }
+        self.updated_relay_enabled = enable;
+    }    
 }
+
--- src/rs/src/taist.rs
+++ src/rs/src/taist.rs
@@ -24,88 +24,43 @@ use core::str;
 use mcp4725::MCP4725;
 
 use crate::i2c_rs_task::power_supply_peripheral;
-
+use crate::i2c_rs_task::peripheral_notify;
+use crate::i2c_rs_task::PeripheralEvent;
 /**
  *  A => shut volt / 10.2 => amp
  *  102 mOhm
  * 
  * 
  */
-
 pub fn test_me()
 {
+    let mut x : main_loop = main_loop {} ;
+    x.run();
+}
+struct main_loop
+{
 
+}
+impl peripheral_notify for main_loop
+{
+    fn notify(&self, event :  PeripheralEvent)
+    {
+
+    }
+}
+impl main_loop
+{
+    pub fn run(&mut self)
+    {
     // create the i2c fake task
-    let tsk : power_supply_peripheral = power_supply_peripheral::new();
-
-/*
-    // PC84
-    let mut pcf : PC8754 = PC8754::new(PS_I2C_INSTANCE, IO_EXPANDER_ADDRESS as u8);
-
-    pcf.write(IO_EXPANDER_DC_ENABLE, false); // DC/DC is active low
-    pcf.write(IO_EXPANDER_RELAY_ENABLE,false);
-*/
+    let tsk : power_supply_peripheral = power_supply_peripheral::new(self);
     let mut led : rnPin  = rnPin::PC13;
     rnGpio::pin_mode(led, rnGpio::rnGpioMode::lnOUTPUT);
     loop
-    {
-        rnGpio::digital_toggle(led);
-        rnDelay(1000);
+        {
+            rnGpio::digital_toggle(led);
+            rnDelay(1000);
+        }
     }
-
-   /*
-
-    // INA
-    let mut string_buf: String<64>  =  String::new();
-    let mut ina : INA219 = INA219::new(PS_I2C_INSTANCE as usize, INA219_ADDRESS as u8,  100*1000, INA219_SHUNT_VALUE ,3);
-
-    let mut mcp475 : MCP4725 = MCP4725::new( PS_I2C_INSTANCE as usize, MCP4725_ADDRESS , 100*1000);
-
-    mcp475.set_voltage(1024);
-
-
-    loop
-    {
-        let mut a = ina.get_shunt_voltage_raw();
-        let mut b = ina.get_current_ma();
-        if b> INA219_OFFSET
-        {
-            b-= INA219_OFFSET;
-        }else
-        {
-            b=0;
-        }
-        if(b<INA219_MIN)
-        {
-            b=0;
-        }
-        let mut v = (ina.get_voltage_v()*1000.) as usize;
-        string_buf.clear();
-        uwrite!(&mut string_buf, "V:{}  Araw:{}  B:{}\n",v,a,b).unwrap();
-        rnLogger(&string_buf);
-
-    }
-
-    loop
-    {
-        let volt = ina.get_voltage_v();
-        string_buf.clear();
-        //uwrite!(&mut string_buf, "Volt:{}\n",(1000.*volt) as usize).unwrap();
-       // rnLogger(&string_buf);
-
-        let mut milli_amp=ina.get_current_ma();
-        if(milli_amp>INA219_OFFSET)
-        {
-            milli_amp=milli_amp-INA219_OFFSET;
-        }
-        if(milli_amp<INA219_MIN)
-        {
-            milli_amp=0;
-        }
-        uwrite!(&mut string_buf, "Amp:{}\n",milli_amp).unwrap();
-        rnLogger(&string_buf);
-        
-    }
-    */
-
 }
+// EOF
